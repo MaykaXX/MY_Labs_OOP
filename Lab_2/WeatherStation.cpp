87528dc76ae257df95e852bac43ad4bb00119af1
Maria Vodko <163645840+MaykaXX@users.noreply.github.com>
2026-03-01 17:46:24 +0200

    add in main.cpp menu
    Signed-off-by: Maria Vodko <163645840+MaykaXX@users.noreply.github.com>

--- Lab_2/WeatherStation.cpp
+++ Lab_2/WeatherStation.cpp
@@ -17,40 +17,36 @@ void WeatherStation::add_Sensor(int id, std::string type) {
     sensors.push_back(Sensor(id, type, 0.0));
 };
 
-void WeatherStation::collest_data()
-{
-    for (auto &sensor : sensors) {
+void WeatherStation::collest_data() {
+    for (auto &sensor: sensors) {
         double value = sensor.read_value();
 
         measurements.push_back(Measurement("01.05.2015", value, sensor.get_id()));
     }
 };
 
-void WeatherStation::show_Sensors()
-{
-    for (auto &sensor : sensors) {
+void WeatherStation::show_Sensors() {
+    for (auto &sensor: sensors) {
         std::cout << sensor.get_id() << std::endl;
     }
 };
 
-void WeatherStation::show_history()
-{
-    for (auto &measurement : measurements) {
-         measurement.print();
+void WeatherStation::show_history() {
+    for (auto &measurement: measurements) {
+        measurement.print();
     }
 };
 
-double WeatherStation::calculate_average(const std::string type)
-{
+double WeatherStation::calculate_average(const std::string type) {
     double sum = 0;
     int count = 0;
 
-    for (auto &measurement : measurements) {
+    for (auto &measurement: measurements) {
         int id = measurement.getSensorId();
 
-        for (auto &sensor : sensors) {
+        for (auto &sensor: sensors) {
             if (sensor.get_id() == id && sensor.get_type() == type) {
-                sum+=measurement.getValue();
+                sum += measurement.getValue();
                 count++;
                 break;
             }
@@ -60,5 +56,5 @@ double WeatherStation::calculate_average(const std::string type)
         return 0;
     }
 
-    return sum/count;
+    return sum / count;
 };
---
+++ Lab_2/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+#include "WeatherStation.h"
+
+
+int Sleep(int _cpp_par_);
+
+int main() {
+    srand(time(NULL));
+    WeatherStation wstation("Station A");
+    int choice;
+
+    do {
+        ///////MENU///////
+        std::cout << "\n ***Mayka's weather station Menu***\n";
+        std::cout << "1. Add Sensor\n";
+        std::cout << "2. Show Sensors\n";
+        std::cout << "3. Collect Data\n";
+        std::cout << "4. Show History\n";
+        std::cout << "5. Calculate Average by Type\n";
+        std::cout << "6. Exit\n";
+        //////////////////
+        std::cout << "Choose an option: ";
+        std::cin >> choice;
+
+        switch (choice) {
+            case 1: {
+                int id;
+                std::string type;
+                std::cout << "Enter sensor ID: ";
+                std::cin >> id;
+                std::cout << "Enter sensor type: ";
+                std::cin >> type;
+                wstation.add_Sensor(id, type);
+                break;
+            }
+            case 2: {
+                wstation.show_Sensors();
+                break;
+            }
+            case 3: {
+                wstation.collest_data();
+                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+                std::cout << "Data collected!\n";
+                break;
+            }
+            case 4: {
+                wstation.show_history();
+                break;
+            }
+            case 5: {
+                std::string type;
+                std::cout << "Enter sensor type to calculate average: ";
+                std::cin >> type;
+                double average = wstation.calculate_average(type);
+                std::cout << "Average for type " << type << ": " << average << "\n";
+                break;
+            }
+        }
+    } while (choice != 6);
+    return 0;
+}
